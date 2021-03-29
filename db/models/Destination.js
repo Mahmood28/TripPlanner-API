d7c875f1135a19857f5a32875344937083bb9620
JannatJameel <j_annat@outlook.com>
2021-03-29 10:40:36 +0300

    edited destination model

--- db/models/Destination.js
+++ db/models/Destination.js
@@ -2,6 +2,12 @@ module.exports = (sequelize, DataTypes) => {
   return sequelize.define(
     "Destination",
     {
+      city: {
+        type: DataTypes.STRING,
+      },
+      country: {
+        type: DataTypes.STRING,
+      },
       latitude: {
         type: DataTypes.FLOAT,
       },
