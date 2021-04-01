81ce3e68302f564ce4f29f838c3e08a30be9d1f1
Mahmood28 <mahmoodalwatani@gmail.com>
2021-04-01 17:40:42 +0300

    activities controller

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,32 +1,15 @@
 const { Activity, Destination } = require("../db/models");
 const { amadeus } = require("../config/keys");
 
-exports.placesList = async (req, res, next) => {
-  try {
-    amadeus.referenceData.locations.pointsOfInterest
-      .get({
-        latitude: 52.490569,
-        longitude: 13.457198,
-      })
-      .then(function (response) {
-        res.json(response.data);
-      })
-      .catch(function (response) {
-        console.error(response);
-      });
-  } catch (error) {
-    next(error);
-  }
-};
-
 exports.searchActivities = async (req, res, next) => {
   try {
+    const { country, city, latitude, longitude } = req.body;
     const [destination, created] = await Destination.findOrCreate({
       where: {
-        country: req.body.country,
-        city: req.body.city,
-        latitude: req.body.latitude,
-        longitude: req.body.longitude,
+        country,
+        city,
+        latitude,
+        longitude,
       },
     });
     //if destination created, then we need to request api
@@ -45,9 +28,10 @@ exports.searchActivities = async (req, res, next) => {
 
 const createActivities = async (destination, res) => {
   try {
+    const { latitude, longitude } = destination;
     const activitiesList = await amadeus.shopping.activities.get({
-      latitude: destination.latitude,
-      longitude: destination.longitude,
+      latitude,
+      longitude,
       radius: 20,
     });
     const activities = activitiesList.data.map((activity) => {
@@ -57,17 +41,18 @@ const createActivities = async (destination, res) => {
         destinationId: destination.id,
       };
     });
-    const newactivities = await Activity.bulkCreate(activities);
-    res.json(newactivities);
+    const newActivities = await Activity.bulkCreate(activities);
+    res.json(newActivities);
   } catch (error) {
     console.log(error);
   }
 };
 
-exports.activitiesList = async (req, res, next) => {
+exports.listActivities = async (req, res, next) => {
   try {
+    const { destinationId } = req.params;
     const activities = await Activity.findAll({
-      where: { destinationId: req.query.id },
+      where: { destinationId },
     });
     res.json(activities);
   } catch (error) {
